79db493a98202107e7e3aef2b32107ad44ede273
shawn kearney <spk.vfx@gmail.com>
2018-08-23 23:44:25 -0600

    sk

---
+++ 120-work/hw-3/sketch.js
@@ -0,0 +1,15 @@
+
+
+function setup() {
+
+    createCanvas(600, 400);
+    background('pink');
+}
+
+function draw() {
+    //i made my text a little larger
+    textSize(60);
+    //so I had to drop the y-position some
+    text('hello world', 20, 80);
+
+}
