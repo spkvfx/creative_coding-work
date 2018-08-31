4db07ffc93189a61fb973a25a79aeeec1648c333
spkvfx <spk.vfx@gmail.com>
2018-08-31 00:00:08 -0600

    sk

--- 120-work/hw-4/sketch.js
+++ 120-work/hw-4/sketch.js
@@ -1,51 +1,66 @@
 let bg;
-let myEye;
+let myEye = [];
 
 
 function setup() {
-    bg = color(210,210,215);
+    bg = color(210, 210, 215);
     createCanvas(800, 800);
-    myEye = new Eyeball(50,2,1.5);
 
+    for (let i = 0; i < 100; i+=10) {
+            myEye[i] = new Eyeball(random(10,50),random(1,2),random(1,4),random(0,800),random(0,800));
+    }
 }
+
 
 function draw() {
 
     background(bg);
 
 
-    noFill();
+    text(myEye.length, 400,400);
 
-    translate(50,50) ;
-    myEye.display() ;
+    for (let i = int(0); i < myEye.length; i++) {
+        myEye[i].display();
+    }
+
+
+
 
 
 }
 
-function Eyeball(d,dr,pr) {
+function Eyeball(d,dr,pr,x=0,y=0) {
 
     this.d = d;                                     //eye diameter
     this.dr = dr;                                   //aspect ratio
     this.pr = max(1,pr);                            //pupil ratio (inverse)
 
+    this.x = x;                                     //init x pos
+    this.y = y;                                     //init y pos
 
-    this.display = function(x,y) {
+    this.display = function(tx=0, ty=0) {
+
+        this.tx = tx;                               //transform x (primarily for animation)
+        this.ty = ty;                               //transform y
 
         let w = this.d;                             //height
         let h = this.d/this.dr;                     //width
 
         let p = this.d / (this.dr * this.pr);       //pupil size
 
-        push() ;                                    //main eye sandbox
+        let pos_x = this.x + this.tx ;              //composite position x
+        let pos_y = this.y + this.ty ;              //composite position y
+
+        push() ;                                    //main eye style
         fill(bg) ;                                  //eyelid color
-        arc(x,y,w,h,0,PI) ;                         //eyelid (scaled)
+        arc(pos_x,pos_y,w,h,0,PI) ;                       //eyelid (scaled)
 
         push();                                     //pupil sandbox
         fill("black");                              //pupil color
-        ellipse(x,y,p,p);                           //pupil
-        pop();                                      // push back to main sandbox
+        ellipse(pos_x,pos_y,p,p);                         //pupil
+        pop();                                      //push back to main style
 
-        arc(x,y,w,h,PI,PI,CHORD) ;                  //lower socket
-        pop() ;                                     //restore
+        arc(pos_x,pos_y,w,h,PI,PI,CHORD) ;          //lower socket
+        pop() ;                                     //restore style
     }
 }
