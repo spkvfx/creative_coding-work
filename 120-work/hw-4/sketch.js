083cac984f73fa915df6ef4b7e62aa53921139a1
spkvfx <spk.vfx@gmail.com>
2018-09-05 21:24:31 -0600

    sk

--- 120-work/hw-4/sketch.js
+++ 120-work/hw-4/sketch.js
@@ -2,8 +2,8 @@ let bg;
 let myEye = [];
 
 let myMouth ;
-
 let myBeard;
+
 
 let spacing;
 let h;
@@ -20,55 +20,54 @@ function setup() {
     h = 300;
     v = 300;
 
+    //the array of eyeballs
     let i = 0;
     for (let x = 0; x <= h/spacing; x++) {
         for (let y = 0; y <= v/spacing; y++) {
-            myEye[i] = new Eyeball(random(35, spacing*0.90), random(1, 2), random(1, 4), x*spacing, y*spacing);
+            const m = round(random(0,0.75)) ;
+            myEye[i] = new Eyeball(random(35, spacing*0.90), random(1, 2), random(1, 4), x*spacing, y*spacing, m, spacing) ;
             i++;
         }
     }
 
+    //the mouth
     myMouth = new Mouth(v,100);
-    myBeard = new Beard(v/2,50);
+    //the beard
+    myBeard = new Beard(v,50);
 }
-
-
 
 
 function draw() {
 
     background(bg);
 
-    //primary translate
     push();
-    translate(width/2-h/2,height/2-v/2*1.75);
+        //main translate
+        translate(width/2-h/2,height/2-v/2*1.75);
 
-    //face outline
-    push();
-    noFill();
-    rect(-50,-50,h+100,v+300);
-    fill(bg) ;
+        //face
+        push();
+            noFill();
+            rect(-50,-50,h+100,v+300);
+            fill(bg) ;
 
-    translate(h/4,v*1.75);
-    push();
-    myBeard.display() ;
+            for (let i = int(0); i < myEye.length; i++) {
+                myEye[i].display();
+            }
+
+
+        pop();
+
+        push();
+            myBeard.display(0,525) ;
+        pop() ;
+
+
+        push();
+            myMouth.display(0,v+50);
+        pop();
+
     pop();
-    pop();
-
-
-    for (let i = int(0); i < myEye.length; i++) {
-        myEye[i].display();
-    }
-
-    //mouth
-    myMouth.display(0,v+50);
-    pop();
-
-    pop();
-
-
-
-
 
 }
 
@@ -77,13 +76,16 @@ function Beard(w,h) {
     this.h = h;
 
     this.display = function(tx=0,ty=0) {
+        push() ;
+            translate(tx,ty) ;
+            push() ;
+                fill(bg) ;
+                rect(0,0,this.w,this.h) ;
+                for(let i = 0; i<this.w/5;i++) {
+                    line(i * 5, 0, i * 5, 50);
+                }
+            pop();
         pop();
-        fill(bg) ;
-        push();
-        rect(0,0,this.w,this.h) ;
-        for(let i = 0; i<this.w/5;i++){
-            line(i*5,0,i*5,50);
-        }
 
     }
 }
@@ -94,31 +96,28 @@ function Mouth(w,h) {
     this.h = h;
 
     this.display = function(tx=0, ty=0) {
-        translate(tx,ty);
-
         push();
-        strokeWeight(10);
-        rect(0,0,this.w,this.h,10) ;                    //outline
-        line(0,this.h/2,this.w,this.h/2);          //lips divider
-        pop()
+            translate(tx,ty);
+            push();
+                strokeWeight(10);
+                rect(0,0,this.w,this.h,10) ;               //outline
+                line(0,this.h/2,this.w,this.h/2);          //lips divider
+            pop();
+        pop();
     }
-
-
 }
 
-function Eyeball(d,dr,pr,x=0,y=0) {
+function Eyeball(d,dr,pr,x=0,y=0,m=1,mr=65) {
 
     this.d = d;                                     //eye diameter
     this.dr = dr;                                   //aspect ratio
     this.pr = max(1,pr);                            //pupil ratio (inverse)
+    this.pr = min(1,mr);
 
     this.x = x;                                     //init x pos
     this.y = y;                                     //init y pos
 
     this.display = function(tx=0, ty=0) {
-
-        this.tx = tx;                               //transform x (primarily for animation)
-        this.ty = ty;                               //transform y
 
         let w = this.d;                             //height
         let h = this.d/this.dr;                     //width
@@ -129,17 +128,28 @@ function Eyeball(d,dr,pr,x=0,y=0) {
         let pos_y = this.y;                         //composite position y
 
 
-        push() ;                                    //main eye style
-        translate(tx,ty);
-        fill(bg) ;                                  //eyelid color
-        arc(pos_x,pos_y,w,h,0,PI) ;                       //eyelid (scaled)
+        push() ;                                        //main eye style
+            translate(tx,ty);                           //main translate
+            push() ;
+                fill(bg) ;                              //eyelid color
+                arc(pos_x,pos_y,w,h,0,PI) ;             //eyelid (scaled)
 
-        push();                                     //pupil sandbox
-        fill("black");                              //pupil color
-        ellipse(pos_x,pos_y,p,p);                         //pupil
-        pop();                                      //push back to main style
+                push();                                 //pupil sandbox
+                    fill("black");                      //pupil color
+                    ellipse(pos_x,pos_y,p,p);               //pupil
+                pop();                                      //push back to main style
 
-        arc(this.x,this.y,w,h,PI,PI,CHORD) ;        //lower socket
-        pop() ;                                     //restore style
+                arc(this.x,this.y,w,h,PI,PI,CHORD) ;        //lower socket
+            pop() ;                                         //restore style
+
+            if (m !== 0) {
+                push() ;
+                    noFill() ;
+                    strokeWeight(2.5);
+                    ellipse(this.x,this.y,mr,mr) ;
+                pop() ;
+            }
+
+        pop() ;
     }
 }
