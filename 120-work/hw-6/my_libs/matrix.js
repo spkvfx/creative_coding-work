e4af612f6eefde2dfa83484f9d5645be2b1d858e
spkvfx <spk.vfx@gmail.com>
2018-09-27 23:22:33 -0600

    sk

--- 120-work/hw-6/my_libs/matrix.js
+++ 120-work/hw-6/my_libs/matrix.js
@@ -3,60 +3,74 @@ let err = new Errors() ;
 class Matrix {
 
     constructor(matrix_) {
+        //TODO: maybe add a vector property here rather than convert()
         this.matrix = matrix_;
         return this;
     }
 
-
-    //stolen from:
-    //https://stackoverflow.com/questions/27205018/multiply-2-matrices-in-javascript
-
+    //matrix addition
     add(addend) {
-        let sum = [] ;
+        //verify that the matrix is valid
         if (this.matrix.length !== addend.matrix.length) {
             throw err.mismatch()
         } else {
+            //init the sum array
             let sum = [];
+            //iterate over rows
             for (let row = 0; row <= this.matrix.length - 1; row++) {
+                //init the rows array
                 sum[row] = [];
+                //iterate over columns
                 for (let column = 0; column <= this.matrix[row].length - 1; column++) {
+                    //add components
                     sum[row][column] = this.matrix[row][column] + addend.matrix[row][column];
                 }
             }
         }
+        //create and return the new matrix
         return createMatrix(sum);
     }
 
+    //this was frikkin hard! It was eventually just stolen from:
+    //https://stackoverflow.com/questions/27205018/multiply-2-matrices-in-javascript
     mult(factor) {
+        //init the product
         let product = [];
 
+        //check that the matrix is valid
         const cols = this.matrix[0].length ;
         const rows = factor.matrix.length ;
-
         if (cols !== rows) {
             throw err.composite()
         } else {
-            for (let i = 0; i < this.matrix.length; i++) {
+            //initialize the rows
+            for (let i = 0; i < rows; i++) {
                 product[i] = [];
-                for (let j = 0; j < factor.matrix[0].length; j++) {
+                //get columns
+                for (let j = 0; j < cols; j++) {
                     let sum = 0;
                     for (let k = 0; k < this.matrix[0].length; k++) {
+                        //multiple the rows to the colunmn and sum the column
                         sum += this.matrix[i][k] * factor.matrix[k][j];
                     }
+                    //temp store the result
                     product[i][j] = sum;
                 }
             }
         }
-
+        //create and return the new matrix
         return createMatrix(product);
 
     }
 
+    //convert a 1x3 matrix into a p5 vector
     convert() {
+        //make the the matrix is the right type
         //TODO: return an array of vectors when possible; maybe truncate and warn rather than throw hard error?
         if (this.matrix.length === 0 || this.matrix.length > 1 || this.matrix[0].length < 2 || this.matrix[0].length > 3) {
             throw err.notVector() ;
         } else {
+            //return the vector
             return createVector(this.matrix[0][0], this.matrix[0][1], this.matrix[0][2]) ;
         }
     }
@@ -64,10 +78,12 @@ class Matrix {
 
 class TransformMatrix {
 
+    //I don't totally understand this...
     constructor() {
         return this;
     }
 
+    //identity matrix, just for testing
     ident() {
 
         const matrix = [
@@ -77,8 +93,9 @@ class TransformMatrix {
         ];
         return createMatrix(matrix);
     }
-    ;
 
+    //rotation matrices from wikipedia
+    //rotation along x
     rotX(theta) {
 
         const matrix = [
@@ -89,6 +106,7 @@ class TransformMatrix {
         return createMatrix(matrix);
     };
 
+    //rotation along y
     rotY(theta) {
 
         const matrix = [
@@ -99,6 +117,7 @@ class TransformMatrix {
         return createMatrix(matrix);
     };
 
+    //rotation along z
     rotZ(theta) {
 
         const matrix = [
@@ -111,7 +130,7 @@ class TransformMatrix {
 }
 
 
-
+//callable functions to create classes. not sure if this is remotely the right way to do this, but it works.
 function createMatrix(matrix_) {
     return new Matrix(matrix_) ;
 }
@@ -124,6 +143,7 @@ function createTransform() {
     return new TransformMatrix() ;
 }
 
+//errors for easy access
 function Errors() {
     this.mismatch = function() {
         return Error('invalid matrix: must be same order') ;
--- 120-work/hw-6/sketch.js
+++ 120-work/hw-6/sketch.js
@@ -1,80 +1,146 @@
-let a ;
-let b ;
 
+//get the transform matrices
 let m = createTransform() ;
 
+//initialize the cube
 let myCube ;
 
+//I had this set up a bit differently and needed a
+//let f = 0 ;
+
+//initialize color components, can't figure out how to set color()
+let r;
+let g;
+let b;
+
 function setup() {
-
+    //p5 stuff
     createCanvas(800, 800);
-    frameRate(24) ;
+    frameRate(30) ;
 
-    myCube = new Cube(100);
+    //init colors
+    r = random(0,255) ;
+    g = random(0,255) ;
+    b = random(0,255) ;
+
+    //init cube function
+    myCube = new Cube(100) ;
 
 }
 
 function draw() {
-    background('white')
-    translate(400,400) ;
+    //background to compliment of stroke minus 55
+    background(abs(r-200),abs(g-200),abs(b-200)) ;
 
-    myCube.display(frameCount) ;
+    //angle of rotation in degrees
+    //unnecessary map function. sin(f)*360 would have been fine, but assignment.
+    const theta = map(sin(frameCount*(PI/200)),-1,1,-180,360) ;
+
+    push() ;
+        //translate the cube
+        translate(400,400) ;
+        //stroke color
+        stroke(r,g,b) ;
+        //display the cube at theta degrees
+        myCube.display(theta) ;
+    pop() ;
+
+    //change color in phase
+    if (theta % 180 === 0) {
+        r = random(0,255) ;
+        g = random(0,255) ;
+        b = random(0,255) ;
+    }
 
 }
 
+//this is a mess
 function Cube(r) {
+    //make a point cloud
     let point = [
-        createVector(-r, r, r),  //0
-        createVector(r, r, r),   //1
-        createVector(r, -r, r),  //2
-        createVector(-r, -r, r), //3
+        createVector(-r, r, r),  //front upper left
+        createVector(r, r, r),   //front upper right
+        createVector(r, -r, r),  //front lower left
+        createVector(-r, -r, r), //front lower right
 
-        createVector(-r, r, -r), //4
-        createVector(r, r, -r),  //5
-        createVector(r, -r, -r), //6
-        createVector(-r, -r, -r) //7
+        createVector(-r, r, -r), //rear upper left
+        createVector(r, r, -r),  //rear upper right
+        createVector(r, -r, -r), //rear lower left
+        createVector(-r, -r, -r) //rear lower right
     ];
 
+    //convert the points to 1x3 matrix
+    //TODO: this is not needed. Write the pointcloud into matrix instead
     let matrix = [] ;
-
     for (i = 0; i < point.length; i++) {
         matrix[i] = convertVector(point[i])
-
     }
 
+    //transform points, structure and display the points as a cube
     this.display = function (theta) {
-        let trans = [] ;
+
+        //translate the point cloud
+        let trans = [];
         for (i = 0; i < point.length; i++) {
+            //rotate along the Y axis
             trans[i] = matrix[i].mult(m.rotY(radians(theta)));
+            //rotate 45 degrees along the X axis
             trans[i] = trans[i].mult(m.rotX(radians(45)));
+            //convert the points to vectors (
             point[i] = trans[i].convert();
         }
 
-        //note: vertex winding is likely non-manifold
+        //create topology; vertex order same as point order
+        //note: vertex winding is likely non-manifold, may not be suitable for polygons
+        //TODO: Make points update dynamically so that topology only needs to be established once.
         let vertex = [
-            [point[0], point[1],point[4]],
+            [point[0], point[1], point[4]],
             [point[1], point[2]],
-            [point[2], point[3],point[6]],
+            [point[2], point[3], point[6]],
             [point[3], point[0]],
 
-            [point[4],point[5]],
-            [point[5],point[6], point[1]],
-            [point[6],point[7]],
-            [point[7],point[4], point[3]]
+            [point[4], point[5]],
+            [point[5], point[6], point[1]],
+            [point[6], point[7]],
+            [point[7], point[4], point[3]]
         ];
 
+        //connect vertices with edges
+        //init edges
         let edge = [];
+        //iterate over vertices
         for (let i = 0; i < vertex.length; i++) {
+            //iterate over points
             for (let j = 0; j < vertex[i].length; j++) {
+                //associate points, first point represents vector position
                 if (j > 0) {
                     edge.push([createVector(vertex[i][0].x, vertex[i][0].y, vertex[i][0].z), createVector(vertex[i][j].x, vertex[i][j].y, vertex[i][j].z)]);
                 }
             }
         }
 
+        //draw edges as 2D lines
         let lines = [];
+        //
         for (let i = 0; i < edge.length; i++) {
             lines[i] = line(edge[i][0].x, edge[i][0].y, edge[i][1].x, edge[i][1].y);
         }
-    };
+
+        //display the point coordinates when the mouse is pressed
+        if (mouseIsPressed) {
+            //iterate over verts
+            for (let i = 0; i < vertex.length; i++) {
+                //get the coordinates from the vertex first element
+                const label = [round(vertex[i][0].x), round(vertex[i][0].y), round(vertex[i][0].z)];
+                push();
+                    //get rid of the stroke
+                    noStroke() ;
+                    //draw the text
+                    text(label, vertex[i][0].x + 5, vertex[i][0].y - 5);
+                pop();
+            }
+
+        }
+
+    }
 }
