60a934d73cb65db099ec91e588609165ef4d8184
spkvfx <spk.vfx@gmail.com>
2018-09-29 18:24:03 -0600

    sk

--- 120-work/hw-6/readme.md
+++ 120-work/hw-6/readme.md
@@ -1,3 +1,22 @@
-# Empty p5 sketch.
+# Shawn Kearney
 
-Please replace contents of this file with appropriate readme information after finishing your p5 sketch.
+## Assignment 6 Summary
+
+I've been really interested in 2.5D and 2D projections in p5. I had something else, but I think it will be better suited for a future assignment.
+
+Here, I have this outward spiraly that changes direction depending on `frameCount`. I used modulo to flip flop between clock-side and counter clockwise directions. 
+
+I then projected the x-y, z-y and z-x planes onto the canvas and arranged them into a visually interesting way.
+
+## Steps Taken
+1. I created a position vector and used it's x/y position as the position of an `ellipse`.
+2. Created a velocity vector and updated the position with `vector.add()` and confirmed the results
+3. Applied the cross product between the current position and an up axis vector to the velocity vector and confirmed the resulting spiral shape.
+4. Played around with the velocity expression and added the flip-flopping direct.
+5. I added some animation in the z-axis and applied color to circle
+6. Derived the projections and found an interesting way to compose them.
+
+## Issues
+
+I had some issues with connecting the current points with the previous so that I could scale velocity directly. I tried putting the position into two different variables before and after updating the position, but that did not want to work. A better solution would be substepping with a `for` or `while` function.
+
--- 120-work/hw-6/sketch.js
+++ 120-work/hw-6/sketch.js
@@ -1,41 +1,100 @@
 let y = 0;
 let P;
 let v;
-let up;
+let up ;
 
-let a ;
-let b ;
+let k = 1 ;
+
+let red;
+let blue;
+let green;
+
+const frame_rate = 500 ;
+const period = 4 ;
+
 
 function setup() {
-    frameRate(30) ;
-    createCanvas(800,800) ;
+    //p5 stuff
+    frameRate(frame_rate) ;
+    createCanvas(800,600) ;
+
+    //initialize the position
     P = createVector(0,0,0) ;
-    v = createVector(10,0,0) ;
-    up = createVector(0,0,4) ;
+    //initialize the velocity
+    v = createVector(1,0,0) ;
+
+    //initialize the up vector
+    up = createVector() ;
+
 
 }
 
+//draw the artwork
 function draw() {
 
-    if (frameCount % 90 === 0) {
-        up.x = random(0,1) ;
-        up.y = random(0,1) ;
-        up.z = random(0,1) ;
+    //flipflop the up vector based on period
+    //tried setting up.z while initing up to {0,0,0}, but it didn't work. Not sure why...
+    if (frameCount % frame_rate < random(frame_rate/period,frame_rate)) {
+        up = createVector(0,0,-1) ;
+    } else {
+        up = createVector(0,0,1) ;
     }
 
-    v = v.add(up.normalize().cross(P)).normalize().mult(5) ;
+    //I hope that this satisfies the math functions requirements.
+    //velocity = NORMALIZE(up+(velocity+(up X position)))
+    v = up.add(v.add(up.cross(P)).normalize());
+
+    //get rid of the stroke
     noStroke() ;
-    cd = map(P.z,-800,800,0,255) ;
-    fill(cd,cd,cd) ;
 
-    a = P ;
+    //draw lines using an ellipse
+
+    //draw the x-y plane
+    push() ;
+        //map the color to the position
+        red =     map(P.y % width, -width, width, 0,   255) ;
+        blue =    map(P.z % width, -width, width, 255, 0) ;
+        fill(red,0, blue) ;
+
+        //position drawing
+        translate(width/2,height/4) ;
+
+        //use an ellipse to draw the figure
+        ellipse(P.x,P.y,1,1) ;
+    pop() ;
+
+    //draw the z-y plane
+    push();
+        blue =     map(P.x % width, -width, width, 0,   255) ;
+        green =    map(P.z % width, -width, width, 255, 0) ;
+
+        fill(0,green,blue) ;
+        translate(width,height/2) ;
+        ellipse(P.z % width,P.y,1,1) ;
+    pop();
+
+    //draw the z-x plane
+    push();
+        blue =     map(P.y % width, -width, width, 0,   255) ;
+        green =    map(P.x % width, -width, width, 255, 0) ;
+
+        fill(0,blue,green) ;
+        translate(0,height/2) ;
+        ellipse(-P.z % width,-P.x,1,1) ;
+    pop();
+
+    //reset the position when the y or z position reaches a given value
+    //randomize to the reset threshold value
+    const k = random(-height/10,height/10) ;
+
+    if ((P.y >= height/4 + k || P.x >= height/4 + k) || (P.y <= -height/4 + k || P.x <= -height/4 + 5)) {
+        //reset the position to 0, keep the velocity and up direction
+        P = createVector(0,0,0) ;
+    }
+
+    //update position
     P = P.add(v);
-    b = P ;
 
-    translate(400,400) ;
-    ellipse(P.x,P.y,1,1) ;
 
-    line(a.x,a.y,b.x,b.y) ;
 
-    console.log(a.dist(b)) ;
 }
